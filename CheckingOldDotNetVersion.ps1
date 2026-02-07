080d7a0a2008a2dadae2a01a88644a363f97f935
yannDuchateau <98150537+yannDuchateau@users.noreply.github.com>
2026-02-07 22:54:10 +0100

    Check DotNet Installations

---
+++ CheckingOldDotNetVersion.ps1
@@ -0,0 +1,38 @@
+﻿Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' |
+Where-Object { ($_.PSChildName -ne "v4") -and ($_.PSChildName -like 'v*') } |
+ForEach-Object {
+    $name = $_.Version
+    $sp = $_.SP
+    $install = $_.Install
+    if (-not $install) {
+        Write-Host -Object "$($_.PSChildName)  $($name)"
+    }
+    elseif ($install -eq '1') {
+        if (-not $sp) {
+            Write-Host -Object "$($_.PSChildName)  $($name)"
+        }
+        else {
+            Write-Host -Object "$($_.PSChildName)  $($name) SP$($sp)"
+        }
+}
+    if (-not $name) {
+        $parentName = $_.PSChildName
+        Get-ChildItem -LiteralPath $_.PSPath |
+        Where-Object {
+            if ($_.Property -contains 'Version') { $name = Get-ItemPropertyValue -Path $_.PSPath -Name Version }
+            if ($name -and ($_.Property -contains 'SP')) { $sp = Get-ItemPropertyValue -Path $_.PSPath -Name SP }
+            if ($_.Property -contains 'Install') { $install = Get-ItemPropertyValue -Path $_.PSPath -Name Install }
+            if (-not $install) {
+                Write-Host -Object "  $($parentName)  $($name)"
+            }
+            elseif ($install -eq '1') {
+                if (-not $sp) {
+                    Write-Host -Object "  $($_.PSChildName)  $($name)"
+                }
+                else {
+                    Write-Host -Object "  $($_.PSChildName)  $($name) SP$($sp)"
+                }
+            }
+        }
+    }
+}
