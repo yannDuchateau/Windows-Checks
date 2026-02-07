8b5c5fc9c3664b89e626c7c35411f9d26d35ef35
yannDuchateau <98150537+yannDuchateau@users.noreply.github.com>
2026-02-07 22:48:18 +0100

    Check Physical HDD state With Powershell and winsat

---
+++ PhysicalDisk.ps1
@@ -0,0 +1,3 @@
+﻿Get-PhysicalDisk | Get-StorageReliabilityCounter | Select-Object -Property DeviceID, FriendlyName, Wear, Temperature, TemperatureMax | FT
+winsat disk
+winsat disk -ran -write -drive c
