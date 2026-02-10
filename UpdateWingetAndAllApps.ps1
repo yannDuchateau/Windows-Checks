83f94eee418a4f516213c3e2d149d6147fc84312
yannDuchateau <98150537+yannDuchateau@users.noreply.github.com>
2026-02-10 17:28:26 +0100

    This Script downloads the latest version of winget, and updates all installed Apps.
    This Script downloads the latest version of winget, and updates all installed Apps. Works for Windows 10 and 11. Does not works for Intune-enrolled Computers/laptops.

---
+++ UpdateWingetAndAllApps.ps1
@@ -0,0 +1,5 @@
+﻿(Get-AppxPackage Microsoft.DesktopAppInstaller).Version
+Stop-Process -Name "WindowsPackageManagerServer"
+winget upgrade Microsoft.AppInstaller --accept-source-agreements --accept-package-agreements --include-unknown >$env:PUBLIC\Desktop\Logs\UpgradeAppInstaller.txt
+Start-Process -FilePath "WindowsPackageManagerServer.exe"
+winget upgrade --all --accept-source-agreements --accept-package-agreements --include-unknown  >$env:PUBLIC\Desktop\Logs\PackagesUpdates.txt
