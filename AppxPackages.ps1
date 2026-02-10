17819c805dbcfc617aa89bac36ff5f8ac9817b2c
yannDuchateau <98150537+yannDuchateau@users.noreply.github.com>
2026-02-10 17:45:59 +0100

    Lists installed and preinstalled Apps Packages
    Lists installed and preinstalled Apps Packages in a readable list.

---
+++ AppxPackages.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module Appx
+Import-Module Dism
+Get-AppxPackage -AllUsers | Where PublisherId -eq 8wekyb3d8bbwe | Format-List -Property PackageFullName,PackageUserInformation >$env:PUBLIC\Desktop\Logs\InstalledMSPackages.txt
+DISM /online /get-packages >>$env:PUBLIC\Desktop\Logs\LocalAvailablePackages.txt
+
+Invoke-Item $env:PUBLIC\Desktop\Logs\InstalledMSPackages.txt
+Invoke-Item $env:PUBLIC\Desktop\Logs\LocalAvailablePackages.txt
