76a25b008a182349b63f0b6358c8f49eca38b333
yannDuchateau <98150537+yannDuchateau@users.noreply.github.com>
2026-03-17 19:59:34 +0100

    Downloads and updates The Package installer
    Downloads and updates The Package installer, Essential when the Windows Store is down/out, and winget does not respond.

---
+++ Microsoft.DesktopAppInstaller.ps1
@@ -0,0 +1,6 @@
+﻿# Restore/Repair/update the Store. when WinGet is down.
+Invoke-webrequest https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile C:\Sources\addons\appx\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Stop-Process -Name "WindowsPackageManagerServer"
+Add-ProvisionedAppPackage -online -PackagePath "C:\Sources\addons\appx\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -skiplicense
+Add-AppxPackage -Path "C:\Sources\addons\appx\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+Start-Process -FilePath "WindowsPackageManagerServer.exe"
